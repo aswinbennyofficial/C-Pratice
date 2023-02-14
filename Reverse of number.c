298245661d9a33c7cb6205deb70f6b59cb184e23
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-02-14 11:30:39 +0530

    Create Reverse of number.c

---
+++ Reverse of number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+int a,rev=0,rm;
+printf("Enter the tale of num to print: ");
+scanf("%d",&a);
+
+while(a>0){
+rm=a%10;
+rev=rev*10+rm;
+a/=10;
+}
+
+
+
+
+
+printf("%d",rev);
+
+return 0;
+}
