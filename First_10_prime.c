eb0af90865ec5cc30db632ed8eb457e5975d8573
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-02-21 11:43:03 +0530

    Create First_10_prime.c

---
+++ First_10_prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+int count=0,i,j,div=0;
+
+    for (i=2;i<=100;i++){
+        if(count<10){
+            div=0;
+            for(j=1;j<=i;j++){
+                if(i%j==0){
+                div++;
+                }
+            }
+            if (div<=2){
+            printf("%d\n",i);
+            count++;
+                }
+            }
+    }
+
+}
